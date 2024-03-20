80e6b64752507749e2849ba1ec2e498b773c3d01
Simon Perkins <simon.perkins@gmail.com>
2024-03-20 14:33:28 +0200

    Remove obsolete logger (#23)

--- xarrayfits/__init__.py
+++ xarrayfits/__init__.py
@@ -6,27 +6,4 @@ __author__ = """Simon Perkins"""
 __email__ = "simon.perkins@gmail.com"
 __version__ = "0.2.1"
 
-import logging
-
-
-def __create_logger():
-    # Console formatter, mention name
-    cfmt = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
-
-    # Console handler
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    ch.setFormatter(cfmt)
-
-    logger = logging.getLogger("xarray-fits")
-    logger.handlers = []
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(ch)
-    logger.propagate = False
-
-    return logger
-
-
-log = __create_logger()
-
 from xarrayfits.fits import xds_from_fits  # noqa
